f706af675dd75946ab8c824f6d27b07caed1ce07
Andrew Ingraham <aingraham@about.com>
2015-04-12 13:50:41 -0400

    Broke project into separate files and added some TODOs

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # flame-particles
+
+## TODOs
+### Controls
+#### Particle attributes
+1. Number
+2. Color
+3. Opacity
+4. Decay Rate
+5. Starting position
+6. Rise rate
+
+#### Other attributes
+1. Background color
+
+#### Misc.
+1. Random button that sets all the controls randomly
+2. Follow mouse
+3. Presets
---
+++ fire.js
@@ -0,0 +1,116 @@
+function setConstants() {
+  constants.red = document.getElementById('red').value;
+  constants.green = document.getElementById('green').value;
+  constants.blue = document.getElementById('blue').value;
+  constants.size = document.getElementById('size').value;
+  constants.particleCount = document.getElementById('particles').value;
+  constants.riseSpeed = document.getElementById('riseSpeed').value;
+  constants.xVariance = document.getElementById('xVariance').value;
+  constants.yVariance = document.getElementById('yVariance').value;
+  constants.radianMax = document.getElementById('radianMax').value;
+  constants.weirdMultiplier = document.getElementById('weirdMultiplier').value;
+  constants.decaySpeed = document.getElementById('decaySpeed').value;
+}
+
+function Particle(x, y, size, color, center) {
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.color = color;
+  this.center = center;
+}
+
+Particle.prototype.draw = function() {
+  ctx.strokeStyle = "rgba(255, 255, 255, 0)";
+  ctx.lineWidth = 0;
+
+  this.y -= constants.riseSpeed;
+  this.x += Math.random() * ((this.center - this.x) / 40);
+  //this.x += ((this.center - this.x) / 100);
+  //this.size -= Math.random();
+
+  this.size = Math.max(this.size - (constants.decaySpeed * Math.random()), 0);
+
+  //ctx.fillStyle = "rgba(155, 0, 0, .5)";
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.size, 0, Math.PI*2, true); 
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.stroke();
+};
+var display = document.getElementById('display');
+var ctx = display.getContext('2d');
+var particles = [];
+var width = display.width = window.innerWidth;
+var height = display.height = window.innerHeight;
+var mouse = {
+  x: width / 2,
+  y: height / 2
+};
+
+var constants = {};
+
+setConstants();
+
+/*display.addEventListener('mousemove', function(e) {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});*/
+
+requestAnimationFrame(frame);
+function frame() {
+  requestAnimationFrame(frame);
+  ctx.clearRect(0, 0, width, height);
+  var stillVisibleParticles = [];
+  particles.forEach(function(particle) {
+      particle.draw();
+      if (particle.size > 0) {
+          stillVisibleParticles.push(particle);
+      }
+
+  });
+
+  particles = stillVisibleParticles;
+
+  // TODO - Instead of creating new particles, just reset the old ones.
+  addParticles(particles, Math.min(constants.particleCount / 20, constants.particleCount - particles.length));
+  //addParticles(particles, constants.particleCount - particles.length);
+  //addParticles(particles, Math.min(particles.length + 1, constants.particleCount - particles.length));
+}
+
+function getXAndY() {
+  var degrees = Math.random() * 360,
+      //radians = Math.random() * 25;
+      radians = Math.random() * constants.radianMax;
+
+  return {
+      radians: radians,
+      x: radians * Math.cos(degrees),
+      y: radians * Math.sin(degrees)
+  };
+}
+
+function rcHelper(color, xAndY) {
+  return Math.floor(Math.min(color / xAndY.radians * constants.weirdMultiplier, 255));
+}
+
+function randomColors(xAndY) {
+  return [rcHelper(constants.red, xAndY), rcHelper(constants.green, xAndY), rcHelper(constants.blue, xAndY)].join(',');
+}
+
+function addParticles(particleArray, numToAdd) {
+
+  setConstants();
+  //numToAdd = Math.min(numToAdd, 1);
+  numToAdd = numToAdd / 3;
+
+  for (var i = 0; i < numToAdd; i++) {
+    var xAndY = getXAndY();
+    var color = 'rgba(' + randomColors(xAndY) + ',.1)';
+    
+    //particleArray.push(new Particle(mouse.x + Math.random() - .5) * 50), mouse.y + ((Math.random() - .5) * 50), constants.size));
+    particleArray.push(new Particle( ((Math.random() - .5) * 20) + (mouse.x + xAndY.x), -50 + mouse.y + xAndY.y, constants.size, color, mouse.x));
+  }
+}
